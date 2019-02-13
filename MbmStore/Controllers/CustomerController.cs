8080e070bf00aa07c22d3c7dab56d64e3ebdea6f
Alexandra Dima <alexanddra.dima89@yahoo.com>
2019-02-13 19:57:37 +0100

    Add project files.

---
+++ MbmStore/Controllers/CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MbmStore.Models;
+namespace MbmStore.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            // create a new Movie object with instance name Customer, using the constructures in paranthesis
+            Customer firstCustomer = new Customer("Maria", "Dima", "20", "Jernbanegade 24", "7100", "Vejle");
+            Customer secondCustomer = new Customer("Alexandra", "Dima", "20", "Jernbanegade 30", "7100", "Vejle");
+            Customer thirdCustomer = new Customer("George", "Marian", "20", "Jernbanegade 30", "7100", "Vejle");
+
+            /*
+            List<string> PhoneNumbers = new List<string>()
+            {
+            PhoneNumbers.Add("2222222");
+            PhoneNumbers.Add("2222222");
+        }
+        */
+
+
+            // assign a viewbag property to the new Movie object
+            ViewBag.FirstCustomer = firstCustomer;
+            ViewBag.SecondCustomer = secondCustomer;
+            ViewBag.ThirdCustomer = thirdCustomer;
+
+            return View();
+        }
+
+
+      
+
+    
+
+}
+    }
---
+++ MbmStore/Models/Customer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MbmStore.Models
+{
+    public class Customer
+    {
+        // fields
+        string firstname;
+        string lastname;
+        int age;
+        string address;
+        string zip;
+        string city;
+        List<string> phoneNumbers;
+        DateTime birthDate;
+
+        // /////properties
+        //Auto implemented property, in this case I won't need private string image url.
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        ///Calculate the age of the customer from the birthday
+        public int Age {
+            get {
+                DateTime now = DateTime.Now;
+                int age;
+
+                age = now.Year - birthDate.Year;
+                // calculate to see if the customer hasn’t had birthday yet 
+                // subtract one year if that is so 
+                if (now.Month < birthDate.Month || (now.Month == birthDate.Month && now.Day < birthDate.Day)) ;
+
+            }
+        }
+
+        public string Address { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+
+        public List<string> PhoneNumbers {
+            get { return phoneNumbers; }
+        }
+
+        DateTime Birthdate { get; set; }
+        // constructors 
+
+        public Customer(string firstname, string lastname, int age, string address, string zip, string city)
+        {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Age = age;
+            this.Address= address;
+            this.Zip = zip;
+            this.City = city;
+        }
+        /// method AddPhone
+        public void addPhone(string phone)
+        {
+            AddPhone.Add(phone);
+        }
+    }
+
+}
+}
+
