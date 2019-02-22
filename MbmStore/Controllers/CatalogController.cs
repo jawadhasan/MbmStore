c5a49b7065d187294c5ec3959b8f095d37903761
Alexandra Dima <alexanddra.dima89@yahoo.com>
2019-02-22 16:30:16 +0100

    22.02..

--- MbmStore/Controllers/CatalogController.cs
+++ MbmStore/Controllers/CatalogController.cs
@@ -13,53 +13,28 @@ namespace MbmStore.Controllers
         // GET: Catalog
         public ActionResult Index()
         {
-            // Initiate the Book subclass and create an object for it
-            Book b1 = new Book("Bob Dylan Chronicles", "James Stone", 20, "Books(2015)", "2332323", "ChroniclesBobDilan.jpg");
-            Book b2 = new Book("Miles Autobiography", "Miles Arthur", 30, "Books(2018)", "445667", "MilesAutobiography.jpg");
-            Book b3 = new Book("Revolution in the head", "Nick Stone", 30, "Books(2018)", "445667", "RevolutionInTheHead.jpg");
+           
+        // Initiate the Book subclass and create an object for it
+
+            List<Book> books = new List<Book>();
+            books = Repository.Products.OfType<Book>().ToList();
 
 
-            // Initiate the MusicCD subclass and create an object for it
-            MusicCD cd1 = new MusicCD("With the Beatles", "Beatles", 30, "CDBeatles.png");
-            cd1.addTrack(new Track("I Will", "Richard Rodgers", new TimeSpan(0, 2, 20)));
-            cd1.addTrack(new Track("Blue Moon", "Richard Rodgers", new TimeSpan(0, 2, 52)));
-            cd1.addTrack(new Track("Step Inside Love", "Richard Rodgers", new TimeSpan(0, 2, 15)));
-            cd1.addTrack(new Track("Los Paranoias", "John Lennon", new TimeSpan(0, 2, 05)));
-            cd1.addTrack(new Track("I Will", "Richard Rodgers", new TimeSpan(0, 2, 20)));
-            cd1.addTrack(new Track("Blue Moon", "Richard Rodgers", new TimeSpan(0, 2, 52)));
-            cd1.addTrack(new Track("Step Inside Love", "Richard Rodgers", new TimeSpan(0, 2, 15)));
-            cd1.addTrack(new Track("Los Paranoias", "John Lennon", new TimeSpan(0, 2, 05)));
+            List<MusicCD> musicCD = new List<MusicCD>();
+            musicCD = Repository.Products.OfType<MusicCD>().ToList();
+
+
+
            
 
 
-            MusicCD cd2 = new MusicCD("Bob Dylan - The collection", "Bob Dylan", 30, "BobDilanCD.jpg");
-            cd2.addTrack(new Track("Blowin' in the Wind", "Don Hunstein", new TimeSpan(0,2, 48)));
-            cd2.addTrack(new Track("Girl from the North Country", "Don Hunstein", new TimeSpan(0, 2, 38)));
-            cd2.addTrack(new Track("Down the Highway", "Don Hunstein", new TimeSpan(0, 2, 07)));
-            cd2.addTrack(new Track("Blowin' in the Wind", "Don Hunstein", new TimeSpan(0, 2, 48)));
-            cd2.addTrack(new Track("Girl from the North Country", "Don Hunstein", new TimeSpan(0, 2, 38)));
-            cd2.addTrack(new Track("Down the Highway", "Don Hunstein", new TimeSpan(0, 2, 07)));
-
-            MusicCD cd3 = new MusicCD("The real Miles Davis", "Miles Davis", 30, "MilesDavisCD.jpg");
-            cd3.addTrack(new Track("Down the Highway", "Don Hunstein", new TimeSpan(0, 2, 07)));
-            cd3.addTrack(new Track("I Will", "Richard Rodgers", new TimeSpan(0, 2, 20)));
-            cd3.addTrack(new Track("Blue Moon", "Richard Rodgers", new TimeSpan(0, 2, 52)));
-            cd3.addTrack(new Track("Down the Highway", "Don Hunstein", new TimeSpan(0, 2, 07)));
-            cd3.addTrack(new Track("I Will", "Richard Rodgers", new TimeSpan(0, 2, 20)));
-            cd3.addTrack(new Track("Blue Moon", "Richard Rodgers", new TimeSpan(0, 2, 52)));
-
-
-
             // assign a viewbag property to the new Book object
-            ViewBag.B1 = b1;
-            ViewBag.B2 = b2;
-            ViewBag.B3 = b3;
+            ViewBag.Books = books;
+      
 
             // assign a viewbag property to the new MusicCD object
-            ViewBag.Cd1 = cd1;
-            ViewBag.Cd2 = cd2;
-            ViewBag.Cd3 = cd3;
-
+            ViewBag.MusicCD = musicCD;
+   
 
 
             return View();
